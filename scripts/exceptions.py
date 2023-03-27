3ccf99a9e65f269f20823b67fc391a51577be3ec
ngng628 <ngng628.kyopro@gmail.com>
2023-03-27 20:37:28 +0900

    数式やコードブロックの前後の空白チェックを追加

---
+++ scripts/exceptions.py
@@ -0,0 +1,22 @@
+class TitleTagError(Exception):
+    def __init__(self, msg='タイトルのみ<h2>で囲むべきです'):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
+
+
+class StructureOfStatementError(Exception):
+    def __init__(self, msg='問題文の流れは「問題・制約・入力・出力・入力例1・出力例1・…」であるべきです'):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
+
+
+class SpaceError(Exception):
+    def __init__(self, msg=r'\( \) や <code> </code> の前後には空白を入れるべきです'):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
